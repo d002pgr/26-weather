ed07363b724f9c4daa1f44fddf00b2238d4e5283
d002pgr <D002PGR@gmail.com>
2023-12-06 22:21:37 +0300

    Add files via upload

---
+++ js/constants.js
@@ -0,0 +1,8 @@
+export {inputField, input, cityList, cityTemperature, cityName, toggleHeart}
+
+const inputField = document.querySelector('.input-field');
+const input = document.querySelector('input');
+const cityList = document.querySelector('ul');
+const cityTemperature = document.querySelector('h1');
+const cityName = document.querySelector('.name-bar p');
+const toggleHeart = document.querySelector('.name-bar svg');
---
+++ js/weatherInfo.js
@@ -0,0 +1,16 @@
+export {infoForRequest, getWeather}
+
+const infoForRequest = {
+    'serverUrl': 'http://api.openweathermap.org/data/2.5/weather',
+    'city': 'boston',
+    'apiKey': '4fb30dcd8eb80d796c79e4ba22d7b907'
+};
+
+function getWeather() {
+    const url = `${infoForRequest.serverUrl}?q=${infoForRequest.city}&appid=${infoForRequest.apiKey}&units=metric`;
+    return fetch(url).then(response => response.json())
+}
+
+
+
+
