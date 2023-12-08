4e68a224cc03263c5b9f759c7075e350f5ed4abb
d002pgr <D002PGR@gmail.com>
2023-12-08 10:51:20 +0300

    Delete js directory

--- js/constants.js
+++
@@ -1,8 +0,0 @@
-export {inputField, input, cityList, cityTemperature, cityName, toggleHeart}
-
-const inputField = document.querySelector('.input-field');
-const input = document.querySelector('input');
-const cityList = document.querySelector('ul');
-const cityTemperature = document.querySelector('h1');
-const cityName = document.querySelector('.name-bar p');
-const toggleHeart = document.querySelector('.name-bar svg');
