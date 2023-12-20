00908cc527ddaa5c42f464dcc4f0de4e14ac11e3
d002pgr <D002PGR@gmail.com>
2023-12-20 16:37:37 +0300

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,43 @@
-export { inputField, input, cityList, cityTemperature, heart }
+export { constants, getFormattedTime }
 
-const inputField = document.querySelector('.input-field');
-const input = document.querySelector('input');
-const cityList = document.querySelector('ul');
-const cityTemperature = document.querySelector('h1');
-const heart = document.querySelector('.name-bar svg');
+const constants = { 
+    inputField: document.querySelector('.input-field'),
+    input: document.querySelector('input'),
+
+    cityName: document.querySelector('.name-bar p'), 
+    cityList: document.querySelector('.city-list'),
+    cityTemperature: document.querySelector('h1'),
+
+    weatherIcon: document.querySelector('.weather-icon'),
+    heart: document.querySelector('.name-bar svg'),
+
+    currentFeelsLike: document.querySelector('.current-feels-like'),
+    sunrise: document.querySelector('.sunrise'),
+    sunset: document.querySelector('.sunset'),
+
+    firstPeriod: document.querySelector('.first-period'),
+    firstPeriodTemperature: document.querySelector('.first-period-temperature'),
+    firstPeriodFeelsLike: document.querySelector('.first-period-feels-like'),
+    firstPeriodImg: document.querySelector('.first-period-img'),
+
+    secondPeriod: document.querySelector('.second-period'),
+    secondPeriodTemperature: document.querySelector('.second-period-temperature'),
+    secondPeriodFeelsLike: document.querySelector('.second-period-feels-like'),
+    secondPeriodImg: document.querySelector('.second-period-img'),
+
+    thirdPeriod: document.querySelector('.third-period'),
+    thirdPeriodTemperature: document.querySelector('.third-period-temperature'),
+    thirdPeriodFeelsLike: document.querySelector('.third-period-feels-like'),
+    thirdPeriodImg: document.querySelector('.third-period-img')
+}
+
+function getFormattedTime(timestamp, timezone) {
+    const myDateNow = new Date();
+    const myTimeOffset = myDateNow.getTimezoneOffset() * 60 * 1000;
+    const date = new Date(timestamp * 1000 + myTimeOffset + timezone * 1000);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    return hours + ':' + minutes;
+}
